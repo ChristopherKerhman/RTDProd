470eb182ae57ec06fbccd2bc0a5e52537e1dd267
Calmes Christophe <77333391+ChristopherKerhman@users.noreply.github.com>
2022-06-18 10:31:18 +0200

    Delete paramDB.php
    Info critique

--- objets/paramDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-class ParamDB {
-  protected $serverName = "localhost";
-  protected $userName = "xgyd0647_areshe430";
-  protected $password = "w3iKj)WlyZos";
-  protected $dbName = "xgyd0647_roleTheDice";
-}
